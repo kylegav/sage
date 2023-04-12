ae842acf8980cf146abab0006cd46eae5d30e0b1
kylegav <60828879+kylegav@users.noreply.github.com>
2023-04-12 17:48:34 -0400

    Started implementatiomn on popovers

--- src/Modules/CalendarView/index.js
+++ src/Modules/CalendarView/index.js
@@ -1,10 +1,21 @@
-import {Card, Calendar, Alert} from 'antd';
+import {Card, Calendar, Alert, Popover, Button} from 'antd';
 import React, { useState } from 'react';
 import dayjs from 'dayjs';
 
 const CalendarView = () => {
    const [value, setValue] = useState(() => dayjs(dayjs.today));
    const [selectedValue, setSelectedValue] = useState(() => dayjs(dayjs.today));
+   const [open, setOpen] = useState(false);
+
+   // TODO: Implement Popover element for Calendar
+  const hide = () => {
+    setOpen(false);
+  };
+
+  const handleOpenChange = (newOpen) => {
+    setOpen(newOpen);
+  };
+
 
    const onSelect = (newValue) => {
     setValue(newValue);
