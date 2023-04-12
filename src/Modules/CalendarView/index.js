17ce883aba8bf789770784fe71d2097da0fdc73e
kylegav <60828879+kylegav@users.noreply.github.com>
2023-04-12 15:54:54 -0400

    Added Active Selection to Calendar view

--- src/Modules/CalendarView/index.js
+++ src/Modules/CalendarView/index.js
@@ -1,13 +1,24 @@
-import {Card, Calendar} from 'antd';
+import {Card, Calendar, Alert} from 'antd';
+import React, { useState } from 'react';
+import dayjs from 'dayjs';
 
 const CalendarView = () => {
+   const [value, setValue] = useState(() => dayjs(dayjs.today));
+   const [selectedValue, setSelectedValue] = useState(() => dayjs(dayjs.today));
 
-    const onPanelChange = () =>  {
-      console.log("Calendar Change")
-    }
+   const onSelect = (newValue) => {
+    setValue(newValue);
+    setSelectedValue(newValue);
+   };
+
+   const onPanelChange = (newValue) =>  {
+      console.log(newValue)
+   };
+
     return (
         <Card title={"Calendar Overview"}>
-          <Calendar onPanelChange={onPanelChange} />
+          <Alert message={`Active Selection: ${selectedValue?.format('YYYY-MM-DD')}`} />
+          <Calendar onPanelChange={onPanelChange} onSelect={onSelect} />
         </Card>
     )
 };
