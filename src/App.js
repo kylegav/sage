59f49411c3b5e4fbcb2ce635cbb615ffad113d36
kylegav <60828879+kylegav@users.noreply.github.com>
2023-04-12 18:48:38 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,53 +7,6 @@ import dummydata from "./Data/dummydata.json"
 const {Content, Footer, Header} = Layout;
 
 function App() {
-  
-  const data = dummydata
-  const columns = [
-  {
-    title:'Name',
-    dataIndex:'Name',
-    key: 'employeeID'
-  },
-  {
-    title:'Monday',
-    dataIndex:'Monday',
-    key: 'employeeID'
-  },
-  {
-    title:'Tuesday',
-    dataIndex:'Tuesday',
-    key: 'employeeID'
-  },
-  {
-    title:'Wednesday',
-    dataIndex:'Wednesday',
-    key: 'employeeID'
-  },
-  {
-    title:'Thursday',
-    dataIndex:'Thursday',
-    key: 'employeeID'
-  },
-  {
-    title:'Friday',
-    dataIndex:'Friday',
-    key: 'employeeID'
-  },
-  {
-    title:'Saturday',
-    dataIndex:'Saturday',
-    key: 'employeeID'
-  },
-  {
-    title:'Sunday',
-    dataIndex:'Sunday',
-    key: 'employeeID'
-  }
-]
-
-  
-
 
   return (
       <Layout>
@@ -63,9 +16,6 @@ function App() {
         <Layout>
           <Content>
             <AppRoutes></AppRoutes>
-          </Content>
-          <Content>
-            
           </Content>
           <Footer style={{textAlign: "center", backgroundColor: 'lightgray' }}>
             Sage UI {packageInfo.version} ©Kyle Gavin 2023
