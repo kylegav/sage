537fd2b65e7e07a73584648c1d6a2bc8cca1659e
kylegav <60828879+kylegav@users.noreply.github.com>
2023-04-12 18:52:08 -0400

    fixed approutes

--- src/Components/AppRoutes/index.js
+++ src/Components/AppRoutes/index.js
@@ -4,12 +4,13 @@ import Account from "../../Modules/Account";
 import TimeOff from "../../Modules/TimeOff";
 import Contacts from "../../Modules/Contacts";
 import Employee from "../../Modules/Employee";
+import Calendar from "../../Modules/CalendarView";
 
 const AppRoutes = () => {
     return (
         // TODO: Implement Routing to different pages.
         <Routes>
-            <Route path ="" element={<TableView/>}/>
+            <Route path ="" element={<Calendar/>}/>
             <Route path ="account"  element={<Account/>}/>
             <Route path ="timeoff" element={<TimeOff/>}/>
             <Route path ='contacts' element={<Contacts/>}/>
