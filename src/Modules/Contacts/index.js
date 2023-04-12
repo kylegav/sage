bc53d92f8c83c7e454b5efde2231eeae3badd245
kylegav <60828879+kylegav@users.noreply.github.com>
2023-04-12 02:49:16 -0400

    Added Contacts Card example, with loaded skeleton modules

--- src/Modules/Contacts/index.js
+++ src/Modules/Contacts/index.js
@@ -4,9 +4,11 @@ const {Meta} = Card;
 
 const Contacts = () => {
     return (
+        // TODO: Implement Table View ? Or some Container that allows cards to be displayed
+        // TODO: Implement a Search function.
     <>
     <Card style={{ width: 300, marginTop: 16 , padding: 10}} >
-        <Skeleton>
+        <Skeleton loading={false} avatar active>
         <Meta
             avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" />}
             title="Card title"
