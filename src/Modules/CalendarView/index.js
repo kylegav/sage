c542f0f07b74db8c0ba34684911b45af1e1e43d9
kylegav <60828879+kylegav@users.noreply.github.com>
2023-04-26 15:33:26 -0400

    Added custom cards

--- src/Modules/CalendarView/index.js
+++ src/Modules/CalendarView/index.js
@@ -1,21 +1,23 @@
-import {Card, Calendar, Alert, Popover, Button} from 'antd';
+import {Card, Calendar, Alert, Popover} from 'antd';
 import React, { useState } from 'react';
 import dayjs from 'dayjs';
+import shiftdata from "../../Data/shiftdata.json"
+import shiftdata23 from "../../Data/shiftdata23.json"
+
 
 const CalendarView = () => {
    const [value, setValue] = useState(() => dayjs(dayjs.today));
    const [selectedValue, setSelectedValue] = useState(() => dayjs(dayjs.today));
-   const [open, setOpen] = useState(false);
 
-   // TODO: Implement Popover element for Calendar
-  const hide = () => {
-    setOpen(false);
-  };
-
-  const handleOpenChange = (newOpen) => {
-    setOpen(newOpen);
-  };
-
+   const getShiftCellData = (value) => {
+     let listData;
+     if (value.date() === 26) {
+       listData = shiftdata;
+     } else if (value.date() === 23) {
+       listData = shiftdata23;
+     }
+     return listData || [];
+   };
 
    const onSelect = (newValue) => {
     setValue(newValue);
@@ -26,10 +28,51 @@ const CalendarView = () => {
       console.log(newValue)
    };
 
+   const borderStyles = (Workcenter) => {
+     if (Workcenter === "Bar") {
+       return "blue"
+     } else if (Workcenter === "Wait") {
+       return "red"
+     }
+
+
+   }
+
+   const dateCellRender = (value) => {
+     const listData = getShiftCellData(value);
+
+     const content = (shiftStart, shiftEnd) => {
+       return (
+         <div>
+           <p>{shiftStart} till {shiftEnd}</p>
+         </div>
+       )
+     };
+
+     return (
+       <ul className="shift">
+       {listData.map((item) => (
+         <Popover content={content(item.ShiftStart,item.ShiftEnd)} title={`${item.Name} - ${item.Workcenter}`}>
+           <Card title={`${item.Name} - ${item.Workcenter}`}
+                 size={"small"}
+                 bordered={true}
+                 style={{borderColor: borderStyles(item.Workcenter)}}
+           />
+         </Popover>
+         ))}
+       </ul>
+     );
+   };
+
+
     return (
         <Card title={"Calendar Overview"}>
           <Alert message={`Active Selection: ${selectedValue?.format('YYYY-MM-DD')}`} />
-          <Calendar onPanelChange={onPanelChange} onSelect={onSelect} />
+          <Calendar onPanelChange={onPanelChange}
+                    onSelect={onSelect}
+                    dateCellRender={dateCellRender}
+                    mode={"month"}
+          />
         </Card>
     )
 };
