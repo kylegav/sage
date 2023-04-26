22cbfb99d85916b017c92971699808ec8e406547
Zeizul <99150110+Zeizul@users.noreply.github.com>
2023-04-26 14:49:09 -0500

    Modified contact cards
    color, sizing, features, spacing, text edit, avatar sizing, hyperlink-onclick potential implemented

--- src/Modules/Contacts/index.js
+++ src/Modules/Contacts/index.js
@@ -1,59 +1,80 @@
-import {Card, Avatar, Skeleton, Space} from 'antd';
+import {Card, Avatar, Skeleton} from 'antd';
 import React, { useState } from 'react';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import {FacebookFilled, TwitterOutlined, InstagramOutlined, YoutubeFilled} from "@ant-design/icons";
+import { Pagination } from "antd";
 
 const {Meta} = Card;
 
+
 const Contacts = () => {
     return (
+      
         // TODO: Implement Table View ? Or some Container that allows cards to be displayed
         // TODO: Implement a Search function.
     <>
+      
       <Card
-        style={{ width: 300, marginTop: 16 }}
-        
+        style={{ width: 300, height:150, marginTop: 10, border: 0, background:'linear-gradient(#00cc66,#99ccff)' }}
+        actions={[<FacebookFilled style={{ color: "blue" }}/>,
+         <TwitterOutlined style={{ color: "skyblue" }}/>,
+         <InstagramOutlined style={{color: "purple"}}/>,
+         <YoutubeFilled style={{ color: "red"}}/>,
+        ]}
       >
         <Skeleton loading={false} avatar active>
           <Meta
-            avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=2" />}
-            title="Card title"
-            description="This is the description"
+            avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" />}
+            title="Justin Green"
+            description="4431005000   justin.green@sage.com"
           />
         </Skeleton>
       </Card>
       <Card
-        style={{ width: 300, marginTop: 16 }}
-        
+        style={{ width: 300, height:150, marginTop: 75, border: 0, background:'linear-gradient(#00cc66,#99ccff)' }}
+        actions={[<FacebookFilled style={{ color: "blue" }}/>,
+        <TwitterOutlined style={{ color: "skyblue" }}/>,
+        <InstagramOutlined style={{color: "purple"}}/>,
+        <YoutubeFilled style={{ color: "red"}}/>,
+      ]}
       >
         <Skeleton loading={false} avatar active>
           <Meta
             avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=2" />}
-            title="Card title"
-            description="This is the description"
+            title="Harris White"
+            description="3021007000    harris.white@sage.com"
           />
         </Skeleton>
       </Card>
       <Card
-        style={{ width: 300, marginTop: 16 }}
-        
+        style={{ width: 300, height:150, marginTop: 75, border: 0, background:'linear-gradient(#00cc66,#99ccff)' }}
+        actions={[<FacebookFilled style={{ color: "blue" }}/>,
+        <TwitterOutlined style={{ color: "skyblue" }}/>,
+        <InstagramOutlined style={{color: "purple"}}/>,
+        <YoutubeFilled style={{ color: "red"}}/>,
+      ]}  
       >
         <Skeleton loading={false} avatar active>
           <Meta
-            avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=2" />}
-            title="Card title"
-            description="This is the description"
+            avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=3" />}
+            title="Patunya Picklebottom"
+            description="3026906900     patunya.pickle@sage.com"
           />
         </Skeleton>
       </Card>
       <Card
-        style={{ width: 300, marginTop: 16 }}
-        
+        style={{ width: 300, height:170, marginTop: 75, border: 0, background:'linear-gradient(#00cc66,#99ccff)', fontSize: 15, color: 'white' }}
+        actions={[<FacebookFilled style={{ color: "blue" }}/>,
+        <TwitterOutlined style={{ color: "skyblue" }}/>,
+        <InstagramOutlined style={{color: "purple"}}/>,
+        <YoutubeFilled style={{ color: "red"}}/>,
+      ]}
       >
         <Skeleton loading={false} avatar active>
           <Meta
-            avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=2" />}
-            title="Card title"
-            description="This is the description"
+            avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=4" />}
+            title="Emperor Kuzco"
+            description="3029413502     emperor.kuzco@sage.com"
           />
         </Skeleton>
       </Card>
