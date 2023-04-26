00526f0156da3bbe4f6508bc9af7f613436ff6ce
Prentice Williams <princejakarie@gmail.com>
2023-04-26 02:38:32 -0400

    Added a button for availability requests. Updated the tags to properly render. Updated Date sorter to properly sort.

--- src/Modules/TimeOff/index.js
+++ src/Modules/TimeOff/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 import {
     Card,
+    Button,
     Space,
     Table,
     Tag,
@@ -17,25 +18,18 @@ const columns = [
         title: 'Date',
         dataIndex: 'date',
         key: 'date',
+        sorter: (a,b) => new Date(a.date) - new Date(b.date),
     },
     {
+        //Use column.shouldCellUpdate to update status tag
         title: 'Status',
         dataIndex: 'status',
         key: 'status',
-        render:(_, { tags }) => (
+        render:(status) => (
             <>
-                {tags?.map((tag) => {
-                        let color = tag === 'Available' ? 'geekblue' : 'green';
-                        if (tag === 'Denied') {
-                            color = 'volcano';
-                        }
-                        return (
-                            <Tag color={color} key={tag}>
-                                {tag.toUpperCase()}
-                            </Tag>
-                        );
-                    }
-                )}
+                {status === 'Approved' && <Tag color='green'>{status}</Tag>}
+                {status === 'Denied' && <Tag color='red'>{status}</Tag>}
+                {status === 'Pending' && <Tag color='orange'>{status}</Tag>}
             </>
         ),
     },
@@ -43,23 +37,44 @@ const columns = [
 
 const data = [
     {
-        name: "Joe Shmama",
-        date: "August 12",
-        status: "Available",
+        name: 'Joe Shmama',
+        date: 'August 12',
+        status: 'Approved',
     },
     {
-        name: "Mike Hawk",
-        date: "August 12",
-        status: "Denied",
-    }
+        name: 'Mike Hawk',
+        date: 'August 13',
+        status: 'Denied',
+    },
+    {
+        name: 'Johnny Blaze',
+        date: 'August 9',
+        status: 'Pending',
+    },
 ]
+
 const TimeOff = () => {
+    const [loading, setLoading] = useState(false)
+    const onButtonClick=(e)=>{
+        console.log('Button clicked')
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000);
+    }
     return (
         <>
-            <Card title={"Time Off page"}/>
+            <Button
+                type='primary'
+                loading={loading}
+                icon={<PlusOutlined />}
+                onClick={onButtonClick}
+                >Request Availability</Button>
+            <Card title={"Time Off Page"}/>
             <Table
                 dataSource={data}
                 columns={columns}
+                bordered
                 ></Table>
         </>
     )
